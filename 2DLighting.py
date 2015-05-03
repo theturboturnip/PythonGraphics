26a8e54eea8be79b870b9222ee6a73c02be763fc
The Turbo Turnip <popgoestoast@gmail.com>
2015-05-03 14:54:43 +0100

    Added the ability to set the strength of each degree, and made LOD larger

--- 2DLighting.py
+++ 2DLighting.py
@@ -37,34 +37,38 @@ def calc_bearing(p1,p2):
 
 
 class Light:
-	def __init__(self,pos,strength):
-		self.pos,self.strength=pos,strength
+	def __init__(self,pos,total_strength):
+		self.pos,self.total_strength=pos,total_strength
 		self.lines=[]
-		self.line_thickness=(self.strength/30)
-		self.LOD=self.strength/25
+		self.line_thickness=(self.total_strength/30)
+		self.LOD=self.total_strength/50
+		if self.LOD<1:
+			self.LOD=1
+		self.strength=[]
 		for i in range(0,360):
 			self.lines.append((sin(i),cos(i)))
+			self.strength.append(self.total_strength-random.randint(0,20))
 		self.recalced_without_objects=True
 		self.check_rays([])
 
 	def check_rays(self,objects):
-
 		lines_to_recalc=[]
 		self.rays=[]
 		self.poly=[]
-		for line in self.lines:
-			endpoint=[int(self.pos[0]+(line[0]*self.strength)),int(self.pos[1]+(line[1]*self.strength))]
+		for i in range(len(self.lines)):
+			line=self.lines[i]
+			endpoint=[int(self.pos[0]+(line[0]*self.strength[i])),int(self.pos[1]+(line[1]*self.strength[i]))]
 			self.rays.append([self.pos,endpoint])
 			self.poly.append(endpoint)
 		for obj in objects:
 			bearing=calc_bearing(self.pos,obj)
-			degree_diff=int(20*(1.0-(dist(self.pos,obj)/self.strength)))
+			degree_diff=int(20*(1.0-(dist(self.pos,obj)/self.total_strength)))
 			for i in range(bearing-degree_diff,bearing+degree_diff):
 				lines_to_recalc.append(i)
 		for degree in lines_to_recalc:
 			d=degree-1
-			self.rays[d]=[self.pos,cast_ray(self.lines[d],self.pos,self.strength,objects)]
-			self.poly[d]=cast_ray(self.lines[d],self.pos,self.strength,objects)
+			self.rays[d]=[self.pos,cast_ray(self.lines[d],self.pos,self.strength[d],objects)]
+			self.poly[d]=cast_ray(self.lines[d],self.pos,self.strength[d],objects)
 		if len(objects)==0:
 			self.recalced_without_objects=True
 		else:
@@ -86,20 +90,26 @@ class Window:
 		pygame.init()
 		self.screen=pygame.display.set_mode((WIDTH,HEIGHT))
 		self.objects=[(50,50)]
-		self.lights=[Light((WIDTH/2,HEIGHT/2),25)]
-
+		self.lights=[Light((200,200),200),Light((220,220),100)]
+		self.clock=pygame.time.Clock()		
 	def draw(self):
 		for obj in self.objects:
 			pygame.draw.circle(self.screen,(0,0,0),obj,OBJ_RADIUS,0)
+		self.draw_lights()
+		
+	def draw_lights(self):
+		deltaTime=self.clock.tick(30)/1000
+		surf=pygame.Surface((WIDTH,HEIGHT)).convert_alpha()
 		for light in self.lights:
 			if DRAW_LIGHT_AS_LINES:
 				for line in light.rays:
 					pygame.draw.lines(self.screen,(0,255,0),False,line,light.line_thickness)
 			else:
-				pygame.draw.polygon(self.screen,(0,255,0),light.poly[::light.LOD],0)
+				surf.fill((0,0,0,0))
+				pygame.draw.polygon(surf,(0,255,0,61),light.poly[::light.LOD],0)
+				self.screen.blit(surf, (0,0))
 
 	def recalc_lights(self):
-
 		for light in self.lights:
 			light.check_rays(self.objects)
 
@@ -108,7 +118,6 @@ class Window:
 			for event in pygame.event.get():
 				if event.type==pygame.QUIT:
 					self.quit()
-				
 			self.screen.fill((255,255,255))
 			self.objects[0]=pygame.mouse.get_pos()
 			for l in self.lights:
