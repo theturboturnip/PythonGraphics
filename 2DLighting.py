0a1beaff229a7b824117012d890bd7f859d72439
The Turbo Turnip <popgoestoast@gmail.com>
2015-05-03 11:46:59 +0100

    Did some formatting

--- 2DLighting.py
+++ 2DLighting.py
@@ -5,8 +5,6 @@ HEIGHT=480
 OBJ_RADIUS=5
 DRAW_LIGHT_AS_LINES=False
 
-
-
 def floatrange(start,end,interval):
 	toreturn=[]
 	i=start
@@ -32,12 +30,12 @@ def cast_ray(ray,start_point,length,objects):
 				smallest_length=distance
 	endpoint=[int(start_point[0]+(ray[0]*smallest_length)),int(start_point[1]+(ray[1]*smallest_length))]
 	return endpoint
-
-
 def calc_bearing(p1,p2):
 	xdist=p2[0]-p1[0]
 	ydist=p2[1]-p1[1]
 	return int(math.degrees(math.atan2(xdist,ydist)))
+
+
 class Light:
 	def __init__(self,pos,strength):
 		self.pos,self.strength=pos,strength
@@ -51,7 +49,6 @@ class Light:
 	def check_rays(self,objects,creatinglines=False):
 		objects=self.refine_objs(objects)
 		if len(objects)==0 and not creatinglines and self.recalced_without_objects:
-
 			return 
 		lines_to_recalc=[]
 		self.rays=[]
@@ -73,7 +70,6 @@ class Light:
 			self.recalced_without_objects=True
 		else:
 			self.recalced_without_objects=False
-		#self.rays=lines_to_return
 
 	def refine_objs(self,objects):
 		toreturn=[]
@@ -88,6 +84,7 @@ class Window:
 		self.screen=pygame.display.set_mode((WIDTH,HEIGHT))
 		self.objects=[(50,50)]
 		self.lights=[Light((WIDTH/2,HEIGHT/2),100)]
+
 	def draw(self):
 		for light in self.lights:
 			for line in light.rays:
@@ -97,10 +94,12 @@ class Window:
 					pygame.draw.polygon(self.screen,(0,255,0),light.poly,0)
 		for obj in self.objects:
 			pygame.draw.circle(self.screen,(0,0,0),obj,OBJ_RADIUS,0)
+
 	def recalc_lights(self):
 		self.objects[0]=pygame.mouse.get_pos()
 		for light in self.lights:
 			light.check_rays(self.objects)
+
 	def loop(self):
 		while True:
 			for event in pygame.event.get():
@@ -111,12 +110,9 @@ class Window:
 			self.screen.fill((255,255,255))
 			self.draw()
 			pygame.display.flip()
-
 	
 	def quit(self):
 		pygame.quit();sys.exit()
-
-
 
 w = Window()
 w.loop()
